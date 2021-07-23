26a7cab90552510f76447706669343c98e3a0ef7
unknown <detektiv2202@gmail.com>
2021-07-23 18:14:48 +0300

    readme

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,8 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\UserRequest;
 use App\Models\User;
-use Illuminate\Http\Request;
 
+date_default_timezone_set("Europe/Kiev");
 class UsersController extends Controller
 {
     /**
